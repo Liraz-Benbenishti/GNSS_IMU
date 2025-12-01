- Change output file orientation from radians to degrees - Fix sign error in scale factor updates - Other minor cleanup

src/imu_files.py
@@ -105,5 +105,5 @@ def Write_GNSS_data(filename, out_profile, out_KF_SD):
     outg[:,5] = 20   # nsats
     outg[:,6:14] = 0  # pos covariance
     outg[:,17:23] = 0 # vel covariance
-    outg[:,23:26] = out_profile[:,7:10]  # orientation
+    outg[:,23:26] = np.rad2deg(out_profile[:,7:10])  # orientation
     np.savetxt(filename, outg, header=hdrg, encoding='utf-8', fmt=fmtg, delimiter=' ', comments='')

src/imu_math.py
@@ -386,10 +386,9 @@ def LC_KF_ZUPT_Update(est_C_b_e, est_v_eb_e, meas_omega_ib_b,
     x_est_new = x_est + K @ delta_z
     
     # Covariance update
-    # Simple form
-    #P_new = (np.eye(ns) - K @ H) @ P_propagated
-    # More stable form
-    P_new = (np.eye(ns) - K @ H) @ P_propagated @ (np.eye(ns) - K @ H).T + K @ R @ K.T
+    I = np.eye(ns)
+    #P_new = (I - K @ H) @ P_propagated # Simple form
+    P_new = (I - K @ H) @ P_propagated @ (I - K @ H).T + K @ R @ K.T # More stable form
     
     # Closed-loop correction
     est_C_b_e_new = (np.eye(3) - Skew_symmetric(x_est_new[0:3].flatten())) @ est_C_b_e 
@@ -497,6 +496,9 @@ def Combine_Passes(profiles):
 
 def Zero_Phase_LP(data, cutoff, fs, order=4):
     nyquist = 0.5 * fs
+    if cutoff >= nyquist:
+        print('Warning: Data not filtered, cutoff freq > nyquist')
+        return data
     norm_cutoff = cutoff / nyquist
     b, a = butter(order, norm_cutoff, btype='low', analog=False)
     filtered = filtfilt(b, a, data, axis=0)  # forward-backward filter